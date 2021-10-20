44deb7980e8f250c10acb517e9185f30896f9711
Lukasz Mroz <lukasz.mroz@merck.com>
2021-10-20 08:10:29 +0200

    :heavy_plus_sign: Add webpack config, update ts types

--- src/service/sampleService.ts
+++ src/service/sampleService.ts
@@ -1,18 +1,23 @@
 import { logger } from "../utils/logger";
 import { SampleError } from "../utils/errors";
+import { SampleResponse } from "../types/response";
 
 const LOG_SOURCE = "sampleService";
 
-export const testAction = () => {
+export const testAction = (): SampleResponse => {
   try {
     logger.info("testAtion finished", { source: LOG_SOURCE, method: "GET" });
     return {
       status: 200,
     };
   } catch (e) {
-    throw new SampleError({
-      logSource: LOG_SOURCE,
-      message: e.message,
-    });
+    if (e instanceof Error) {
+      throw new SampleError({
+        logSource: LOG_SOURCE,
+        message: e.message,
+      });
+    } else {
+      throw new Error(String(e));
+    }
   }
 };
