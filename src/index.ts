4bdc99685f83e1db1076e6a0cd5192b863cd50b0
Lukasz Mroz <lukasz.mroz@merck.com>
2021-11-05 15:55:16 +0100

    :heavy_plus_sign:  Setup Eslint and Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import Router from "./controller/Router";
-import { returnError } from "./utils/errorHandler";
-import { logger } from "./utils/logger";
+import express from 'express';
+import Router from './controller/Router';
+import { returnError } from './utils/errorHandler';
+import { logger } from './utils/logger';
 
 const app = express();
 const port = 3000;
 
-app.use("/", Router);
+app.use('/', Router);
 app.use(returnError);
 
 app.listen(port, () => logger.info(`App listening on port: ${port}`));
