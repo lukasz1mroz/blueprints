5c515b996507579f7ff6a2140320747eae172a38
Lukasz Mroz <lukasz.mroz@merck.com>
2021-11-15 09:02:27 +0100

    :construction: Refactor to function component with hooks

--- react-app/src/MainContent.js
+++ react-app/src/MainContent.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './ApiHandler';
 
-const MainContent = (props) => <h1 className="mainContent">This is backend response: {props.data}</h1>;
+const MainContent = () => {
+  const [data, setData] = useState('Empty data');
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    api.sampleCall({ signal: abortController.signal }).then((response) => {
+      setData(response.data.data);
+    });
+
+    return () => {
+      abortController.abort();
+    };
+  }, [data]);
+
+  return (
+    <div>
+      <h1 className="mainContent">This is backend response: {data}</h1>
+    </div>
+  );
+};
 
 export default MainContent;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,33 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import MainContent from './MainContent';
-import api from './ApiHandler';
+// import api from './ApiHandler';
 import './Styles.css';
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.getSampleData = this.getSampleData.bind(this);
-  }
-
-  getSampleData() {
-    api.sampleCall().then((response) => {
-      this.setState({
-        response: response.data.data,
-      });
-    });
-  }
-
-  componentDidMount() {
-    this.getSampleData();
-  }
-
   render() {
     return (
       <HashRouter>
         <Routes>
-          <Route exact path="/" element={<MainContent data={this.state.response} />} />
+          <Route exact path="/" element={<MainContent />} />
         </Routes>
       </HashRouter>
     );
