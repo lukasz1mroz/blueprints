2c6ade277e8fbd8e436641cf0c59b7dd6f549f4c
Lukasz Mroz <lukasz.mroz@merck.com>
2021-11-19 08:16:31 +0100

    :construction: Setup TypeScript in React

--- react-app/src/apiHandler.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = {
-  sampleCall: () => axios.get('http://localhost:3000/test'),
-};
-
-export default api;
--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import MainContent from './MainContent';
-// import api from './ApiHandler';
 import './Styles.css';
 
 class Main extends React.Component {
@@ -10,7 +9,7 @@ class Main extends React.Component {
     return (
       <HashRouter>
         <Routes>
-          <Route exact path="/" element={<MainContent />} />
+          <Route path="/" element={<MainContent />} />
         </Routes>
       </HashRouter>
     );
