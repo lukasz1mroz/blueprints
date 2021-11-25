1aec13bcdcefd34c0d14a481ea15bf2cbfdfd37f
Lukasz Mroz <lukasz.mroz@merck.com>
2021-11-25 08:05:25 +0100

    :construction: Add useMutation for POST

--- node/src/service/sampleService.ts
+++ node/src/service/sampleService.ts
@@ -1,12 +1,12 @@
-import { logger } from "../utils/logger";
-import { SampleError } from "../utils/errors";
-import { SampleResponse } from "../types/response";
+import { logger } from '../utils/logger';
+import { SampleError } from '../utils/errors';
+import { SampleResponse } from '../types/response';
 
-const LOG_SOURCE = "sampleService";
+const LOG_SOURCE = 'sampleService';
 
-export const testAction = (): SampleResponse => {
+export const testAction = (action: string): SampleResponse => {
   try {
-    logger.info("testAtion finished", { source: LOG_SOURCE, method: "GET" });
+    logger.info('testAtion finished', { source: LOG_SOURCE, method: action });
     return {
       status: 200,
     };
