1dd7b9f1e7d918f0e8550678de86a8f79f4c068f
Lukasz Mroz <lukasz.mroz@merck.com>
2021-11-29 08:27:58 +0100

    :construction: Initial Sentry setup

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
 import Router from './controller/Router';
 import { returnError } from './utils/errorHandler';
 import { logger } from './utils/logger';
@@ -6,7 +9,24 @@ import { logger } from './utils/logger';
 const app = express();
 const port = 3000;
 
+Sentry.init({
+  dsn: 'TODO_SENTRY_KEY',
+  integrations: [new Sentry.Integrations.Http({ tracing: true }), new Tracing.Integrations.Express({ app })],
+  tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use('/', Router);
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      if (error.status === 404 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  }),
+);
 app.use(returnError);
 
 app.listen(port, () => logger.info(`App listening on port: ${port}`));
