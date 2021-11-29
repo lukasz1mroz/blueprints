0ba73d78fc32264faad7090baec8a418bfa791ba
Lukasz Mroz <lukasz.mroz@merck.com>
2021-11-29 08:27:58 +0100

    :construction: Initial Sentry setup

--- node/src/service/sampleService.ts
+++ node/src/service/sampleService.ts
@@ -1,8 +1,13 @@
 import { logger } from '../utils/logger';
 import { SampleError } from '../utils/errors';
 import { SampleResponse } from '../types/response';
+import * as Sentry from '@sentry/node';
 
 const LOG_SOURCE = 'sampleService';
+const transaction = Sentry.startTransaction({
+  op: 'test',
+  name: 'My First Test Transaction',
+});
 
 export const testAction = (action: string): SampleResponse => {
   try {
@@ -12,6 +17,8 @@ export const testAction = (action: string): SampleResponse => {
     };
   } catch (e) {
     if (e instanceof Error) {
+      Sentry.captureException(e);
+
       throw new SampleError({
         logSource: LOG_SOURCE,
         message: e.message,
@@ -19,5 +26,7 @@ export const testAction = (action: string): SampleResponse => {
     } else {
       throw new Error(String(e));
     }
+  } finally {
+    transaction.finish();
   }
 };
