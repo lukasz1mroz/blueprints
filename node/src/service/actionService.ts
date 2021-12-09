8022e93d60e0f447f9c2ff5acc2dc7a77ba54123
Lukasz Mroz <lukasz.mroz@merck.com>
2021-12-09 12:13:25 +0100

    :recycle: Refactor actions, add delete token

--- node/src/service/actionService.ts
+++ node/src/service/actionService.ts
@@ -2,11 +2,23 @@ import { logger } from '../utils/logger';
 import { InternalServerError } from '../utils/errors';
 import { GetPostActionResponse } from '../types/response';
 
-const LOG_SOURCE = 'actionService';
+const LOG_SOURCE = 'ActionService';
 
-export const getPostAction = (action: string): GetPostActionResponse => {
+export const getAction = (): GetPostActionResponse => {
   try {
-    logger.info('getPostAtion finished', { source: LOG_SOURCE, method: action });
+    logger.info('Get action finished', { source: LOG_SOURCE });
+    return {
+      data: 'This is successful response',
+      status: 200,
+    };
+  } catch (e) {
+    throw new InternalServerError(LOG_SOURCE, 'Internal Server Error');
+  }
+};
+
+export const postAction = (): GetPostActionResponse => {
+  try {
+    logger.info('Post action finished', { source: LOG_SOURCE });
     return {
       data: 'This is successful response',
       status: 200,
