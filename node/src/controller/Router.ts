74b6c44717dfdb2241b5bf0f73c5e696789282be
Lukasz Mroz <lukasz.mroz@merck.com>
2021-12-09 10:53:01 +0100

    :construction: Add refresh token

--- node/src/controller/Router.ts
+++ node/src/controller/Router.ts
@@ -1,13 +1,14 @@
 import asyncHandler from 'express-async-handler';
 import { Router } from 'express';
-import { loginRoute, errorRoute, getPostActionRoute } from './Controller';
+import { loginRoute, tokenRoute, errorRoute, getPostActionRoute } from './Controller';
 import authMiddleware from '../Middleware/AuthMiddleware';
 
 const router = Router();
 
-router.get('/loginAction', authMiddleware, asyncHandler(loginRoute));
-router.get('/getAction', authMiddleware, asyncHandler(getPostActionRoute('GET')));
-router.post('/postAction', authMiddleware, asyncHandler(getPostActionRoute('POST')));
+router.get('/login', authMiddleware, asyncHandler(loginRoute));
+router.post('/refreshToken', authMiddleware, asyncHandler(tokenRoute));
+router.get('/get', authMiddleware, asyncHandler(getPostActionRoute('GET')));
+router.post('/post', authMiddleware, asyncHandler(getPostActionRoute('POST')));
 router.get('/error', errorRoute);
 
 export default router;
