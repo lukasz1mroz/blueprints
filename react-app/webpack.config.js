d7b2ed1ae9d40fe17e240c135e206a664cb9ad74
Lukasz Mroz <lukasz.mroz@merck.com>
2022-01-03 08:54:14 +0100

    :recycle: Move webpack to react-app

---
+++ react-app/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  entry: {
+    main: path.resolve(__dirname, './src/index.tsx'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+            plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-transform-typescript'],
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?url=false'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+  },
+};
