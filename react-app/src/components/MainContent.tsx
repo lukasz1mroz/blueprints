21c400ebfd73327b77e90e6d440ce1db13583410
Lukasz Mroz <lukasz.mroz@merck.com>
2022-02-04 13:16:29 +0100

    💄 Update posts UI

--- react-app/src/components/MainContent.tsx
+++ react-app/src/components/MainContent.tsx
@@ -1,8 +1,32 @@
-// Check if user is logged
-// Buttons to get post / posts
+import { useEffect, useState } from 'react';
+import useToken from '../hooks/useToken';
+import Post from '../components/Post';
+import api from '../handlers/ApiCalls';
+import '../styles/MainContent.css';
 
 const MainContent = () => {
-  return <h2>This is main content</h2>;
+  const [posts, setPosts] = useState<any>();
+  const { token } = useToken();
+
+  useEffect(() => {
+    console.log('from useEffect');
+    const postsCall = async () => {
+      const apiResponse = await api.getPosts(token as string);
+      setPosts(apiResponse.data.data);
+    };
+    postsCall();
+  }, []);
+
+  console.log(posts);
+
+  return (
+    <div>
+      <h2 className="mainHeader">This is main content</h2>
+      {/* <h3>{posts && posts[0].title}</h3>
+      <p>{posts && posts[0].body}</p> */}
+      {posts && <Post post={posts[0]} />}
+    </div>
+  );
 };
 
 export default MainContent;
--- react-app/src/handlers/ApiCalls.tsx
+++ react-app/src/handlers/ApiCalls.tsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 const api = {
   login: (username: string, password: string) =>
     axios.get('http://localhost:3000/login', { auth: { username: username, password: password } }),
+  getPosts: (authToken: string) =>
+    axios.get('http://localhost:3000/getPosts', { headers: { Authorization: `Bearer ${authToken}` } }),
 };
 
 export default api;
