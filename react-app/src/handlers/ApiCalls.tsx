99e2ce84621b594a017a726eaf8805b8f34abc18
Lukasz Mroz <lukasz.mroz@merck.com>
2022-02-07 07:36:17 +0100

    🔧 Add temp server URLs

--- react-app/src/handlers/ApiCalls.tsx
+++ react-app/src/handlers/ApiCalls.tsx
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 const api = {
   login: (username: string, password: string) =>
-    axios.get('http://localhost:3000/login', { auth: { username: username, password: password } }),
+    axios.get('http://ec2-3-84-185-71.compute-1.amazonaws.com:3000/login', {
+      auth: { username: username, password: password },
+    }),
   getPosts: (authToken: string) =>
-    axios.get('http://localhost:3000/getPosts', { headers: { Authorization: `Bearer ${authToken}` } }),
+    axios.get('http://ec2-3-84-185-71.compute-1.amazonaws.com:3000/getPosts', {
+      headers: { Authorization: `Bearer ${authToken}` },
+    }),
 };
 
 export default api;
