90470aac1de09d0ca756fa3f1f9f00159ee2b73e
Lukasz Mroz <lukasz.mroz@merck.com>
2023-05-05 10:04:06 +0200

    🔨 Update aws config script

--- node/scripts/aws-get-params.sh
+++ node/scripts/aws-get-params.sh
@@ -6,12 +6,12 @@ set +o xtrace
 
 if [ -z "$CONFIG_SOURCE" ]; then
     echo "exit: No CONFIG_SOURCE specified"
-    exit;
+    exit
 fi
 
 if [ -z "$CONFIG_TARGET" ]; then
     echo "exit: No CONFIG_TARGET specified"
-    exit;
+    exit
 fi
 
 if [ -z "$CONFIG_TEMPLATE" ]; then
@@ -25,13 +25,13 @@ echo "CONFIG TARGET: " $CONFIG_TARGET
 
 APPROVAL_PARAMETERS=$(aws ssm get-parameter --name /blueprints/$CONFIG_SOURCE --region us-east-1 --with-decryption | jq --raw-output '.Parameter.Value')
 
-VARS_TO_SUBSTITUTE=$(echo "$APPROVAL_PARAMETERS" | jq -r "to_entries|map(\"$\(.key)\")| join(\", \")" )
+VARS_TO_SUBSTITUTE=$(echo "$APPROVAL_PARAMETERS" | jq -r "to_entries|map(\"$\(.key)\")| join(\", \")")
 
-for PARAM in $(echo "$APPROVAL_PARAMETERS" | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
+for PARAM in $(echo "$APPROVAL_PARAMETERS" | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]"); do
     export ${PARAM}
 done
 
-TEMPLATE_FILE=$(cd ../config && ls | grep dist-env.$CONFIG_TEMPLATE.*)
-TARGET_FILE=$(cd ../config && ls | grep ^env.$CONFIG_TARGET.*)
+TEMPLATE_FILE=$(cd ./config && ls | grep dist-env.$CONFIG_TEMPLATE.*)
+TARGET_FILE=env.$CONFIG_TARGET.ts
 
-envsubst "${VARS_TO_SUBSTITUTE}" < ../config/$TEMPLATE_FILE > ../config/$TARGET_FILE
+envsubst "${VARS_TO_SUBSTITUTE}" <./config/$TEMPLATE_FILE >./config/$TARGET_FILE
