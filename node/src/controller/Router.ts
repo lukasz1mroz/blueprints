d3054fd1ed6c9a84faeddbad1f119e0529677d18
Lukasz Mroz <lukasz1mroz@gmail.com>
2023-05-08 12:06:11 +0200

    🚚 Update auth routes

--- node/src/controller/Router.ts
+++ node/src/controller/Router.ts
@@ -1,14 +1,15 @@
 import asyncHandler from 'express-async-handler';
 import { Router } from 'express';
-import { loginRoute, tokenRoute, errorRoute, getActionRoute, postActionRoute } from './Controller';
+import { userRoute, tokenRoute, errorRoute, getActionRoute, postActionRoute } from './Controller';
 import authMiddleware from '../middleware/AuthMiddleware';
 import logMiddleware from '../middleware/LogMiddleware';
 
 const router = Router();
 
-router.get('/login', logMiddleware, authMiddleware, asyncHandler(loginRoute));
-router.post('/token', logMiddleware, authMiddleware, asyncHandler(tokenRoute));
-router.delete('/token', logMiddleware, authMiddleware, asyncHandler(tokenRoute));
+router.post('/register', logMiddleware, authMiddleware, asyncHandler(userRoute));
+router.get('/login', logMiddleware, authMiddleware, asyncHandler(userRoute));
+router.get('/refreshToken', logMiddleware, authMiddleware, asyncHandler(tokenRoute));
+router.delete('/removeToken', logMiddleware, authMiddleware, asyncHandler(tokenRoute));
 router.get('/getPosts', logMiddleware, authMiddleware, asyncHandler(getActionRoute));
 router.get('/getPost/:postId', logMiddleware, authMiddleware, asyncHandler(getActionRoute));
 router.post('/post', logMiddleware, authMiddleware, asyncHandler(postActionRoute));
