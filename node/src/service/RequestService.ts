0a92e596c7fec3dfa0cc67c47e35e5666aac3be8
Lukasz Mroz <lukasz1mroz@gmail.com>
2023-05-21 08:13:38 +0200

    ♻️ Update error types in RequestService

--- node/src/service/RequestService.ts
+++ node/src/service/RequestService.ts
@@ -1,6 +1,6 @@
 import axios, { Method } from 'axios';
 import { UnauthorizedError, BadGatewayError } from '../types/errors';
-import { CREATED, NO_CONTENT, OK, UNAUTHORIZED } from 'http-status-codes';
+import { StatusCodes } from 'http-status-codes';
 
 const LOG_SOURCE = 'networkService';
 
@@ -9,10 +9,10 @@ export const request = async (method: Method, url: string, param?: string) => {
 
   const result = await axios(fullUrl, { method: method });
 
-  if (result.status === UNAUTHORIZED) {
+  if (result.status === StatusCodes.UNAUTHORIZED) {
     throw new UnauthorizedError({ logSource: LOG_SOURCE, details: result });
   }
-  if (![OK, CREATED, NO_CONTENT].includes(result.status)) {
+  if (![StatusCodes.OK, StatusCodes.CREATED, StatusCodes.NO_CONTENT].includes(result.status)) {
     throw new BadGatewayError({ logSource: LOG_SOURCE, details: result });
   }
 
