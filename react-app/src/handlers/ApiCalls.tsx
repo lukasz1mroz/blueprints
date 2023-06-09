1792ad979f90c2457efed9c33bf234dfb3542c29
Lukasz Mroz <lukasz1mroz@gmail.com>
2023-06-09 08:08:28 +0200

    🎨 Change frontend proxy routing

--- react-app/src/handlers/ApiCalls.tsx
+++ react-app/src/handlers/ApiCalls.tsx
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const api = {
   login: (username: string, password: string) =>
-    axios.get('http://ec2-3-84-185-71.compute-1.amazonaws.com:3000/login', {
+    axios.get('/api/login', {
       auth: { username: username, password: password },
     }),
   getPosts: (authToken: string) =>
-    axios.get('http://ec2-3-84-185-71.compute-1.amazonaws.com:3000/getPosts', {
+    axios.get('/api/getPosts', {
       headers: { Authorization: `Bearer ${authToken}` },
     }),
 };
