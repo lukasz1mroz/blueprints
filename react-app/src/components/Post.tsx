be86eea4200be5e45ef178775c1cfda795400fff
Lukasz Mroz <lukasz1mroz@gmail.com>
2023-06-29 15:42:01 +0200

    ✨ Add post filtering

--- react-app/src/components/Post.tsx
+++ react-app/src/components/Post.tsx
@@ -7,6 +7,7 @@ import "../styles/Post.css";
 const Post = () => {
   let auth = useAuth();
   const [posts, setPosts] = useState<any>();
+  const [filteredPosts, setFilteredPosts] = useState<any>();
   const [, setError] = useState<any>();
 
   const postsCall = async () => {
@@ -18,14 +19,31 @@ const Post = () => {
     }
   };
 
+  const filterPosts = (e: any) => {
+    //e.preventDefault();
+    const filteredPosts = posts.filter((post: { title: string }) =>
+      post.title.includes(e.key)
+    );
+    console.log("filteredPosts: ", filteredPosts);
+    setFilteredPosts(filteredPosts);
+  };
+
   useEffect(() => {
     postsCall();
   }, []);
 
   return (
     <div className="postWrapper">
+      <input
+        id="posts"
+        aria-label="Search titles"
+        placeholder="Search posts"
+        type="search"
+        name="title"
+        onKeyDown={(e) => filterPosts(e)}
+      />
       {posts &&
-        posts.map((post: { title: string; body: string }, idx: any) => (
+        filteredPosts.map((post: { title: string; body: string }, idx: any) => (
           <div key={idx}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
